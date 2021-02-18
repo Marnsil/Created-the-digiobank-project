6a69e71e0bb180d33e34dcf0adec78282e79e7c2
Marcos <marcos_zoo@hotmail.com>
2021-02-18 10:47:21 -0300

    update to apply the private modifier to set function of cpf propety

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -3,10 +3,12 @@ package one.digitalinnovation.digiobank
 class Pessoa {
     var nome:String = "marcos"
     var cpf:String = "123.123.123-01"
+    private set
 }
-fun main (){
+fun main() {
    val marcos = Pessoa ()
 
+    println(marcos)
     println(marcos.nome)
     println(marcos.cpf)
 
