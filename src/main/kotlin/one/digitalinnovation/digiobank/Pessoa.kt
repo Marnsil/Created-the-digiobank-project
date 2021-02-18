8512841f51d2ee7011576b18abc21b6d247796b9
Marcos <marcos_zoo@hotmail.com>
2021-02-18 00:04:17 -0300

    created person class

---
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digiobank
+
+class Pessoa {
+    var nome:String = "marcos"
+    var cpf:String = "123.123.123-01"
+}
+fun main (){
+   val marcos = Pessoa ()
+
+    println(marcos.nome)
+    println(marcos.cpf)
+
+}
