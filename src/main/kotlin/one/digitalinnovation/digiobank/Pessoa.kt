4898a4582e397a66133e98399f53d1bfefda1f47
Marcos <marcos_zoo@hotmail.com>
2021-02-18 11:01:35 -0300

    update to talk about class member

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -4,12 +4,14 @@ class Pessoa {
     var nome:String = "marcos"
     var cpf:String = "123.123.123-01"
     private set
+
+    constructor()
+
+    fun pessoainfo() = "$nome e $cpf"
+
 }
 fun main() {
    val marcos = Pessoa ()
-
-    println(marcos)
-    println(marcos.nome)
-    println(marcos.cpf)
+    println(marcos.pessoainfo())
 
 }
